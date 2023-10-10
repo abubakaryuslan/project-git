9d8cdb28bd5973e77e8692018140ba24d196ea2a
Muhammad Yuslan Abu Bakar <abubakar.yuslan@gmail.com>
2023-10-10 21:28:18 +0700

    Revert "adding power calculation"
    This reverts commit 072b34e8e5177a11cdba3ead9e66d5989cf443b4.

--- calculator.py
+++ calculator.py
@@ -15,24 +15,19 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-# This function power two numbers
-def power(x, y):
-    return x ** y
-
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-print("5.Power")
 
 while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4', '5'):
+    if choice in ('1', '2', '3', '4'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -51,9 +46,6 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
-        
-        elif choice == '5':
-            print(num1, "**", num2, "=", power(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
